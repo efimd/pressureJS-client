b5ba519ac193db2b7e85d46ea0dbacf0c887bb93
Efim Dimenstein <efim@efimd.com>
2014-03-30 14:39:24 +0300

    initial commit

---
+++ pressureJS-client.js
@@ -0,0 +1,66 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: efim
+ * Date: 3/30/14
+ * Time: 12:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var WebSocket = require('ws');
+
+var utils = require('./lib/utils');
+
+var ws = new WebSocket('ws://localhost:8080');
+
+var config = {};
+var statInterval;
+
+ws.on('open', function() {
+
+    var msg = {
+        type : 'init',
+        ip : utils.getLocalIPv4(),
+        hostname : utils.gethostname(),
+        info : utils.getSystemInfo()
+    };
+
+    ws.send(JSON.stringify(msg));
+});
+ws.on('message', function(data, flags) {
+    console.log(data);
+    var msg = {};
+    try {
+        msg = JSON.parse(data);
+    }
+    catch (e) {
+        console.error('Error parsing message: '+ data + ' e:'+e);
+    }
+
+    if (msg.type == 'initConfig') {
+        config = msg.config;
+        init(config);
+
+        ws.send(JSON.stringify({
+            type : 'ready'
+        }));
+    }
+
+});
+
+ws.on('close', function () {
+    clearInterval(statInterval);
+});
+
+function init(cfg) {
+    if (cfg.stat_period) {
+        statInterval = setInterval(reportStats, cfg.stat_period);
+    }
+}
+
+function reportStats() {
+    ws.send(JSON.stringify({
+        type : 'sysStats',
+        stats : utils.getSystemStats()
+    }));
+}
